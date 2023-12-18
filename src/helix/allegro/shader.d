62e6277611097355100fef29404c22d0b4bd5b94
Martijn van Iersel <mvaniersel@gmail.com>
2023-12-18 12:34:04 +0100

    Add wrapper for ALLEGRO_SHADER*

---
+++ src/helix/allegro/shader.d
@@ -0,0 +1,94 @@
+module helix.allegro.shader;
+
+import helix.allegro.bitmap;
+import allegro5.allegro;
+import allegro5.shader;
+import std.string : toStringz;
+import std.format : format;
+import std.conv : to;
+
+
+class ShaderException : Exception {
+	this(string msg, string file= __FILE__, size_t line = __LINE__) {
+		super(msg, file, line);
+	}
+}
+
+/**
+Wrapper and builder for 
+
+ALLEGRO_SHADER*
+al_create_shader
+al_attach_shader_source
+al_build_shader
+
+Checks return codes: Shader compilation errors are turned into ShaderExceptions.
+*/
+class Shader {
+
+	private ALLEGRO_SHADER *shader;
+	
+	ALLEGRO_SHADER* ptr() {
+		return shader;
+	}
+
+	private this() {}
+
+	static Shader ofFragment(string fragmentShaderSource) {
+		const char *vertexShaderSource = al_get_default_shader_source(
+			ALLEGRO_SHADER_PLATFORM.ALLEGRO_SHADER_AUTO, 
+			ALLEGRO_SHADER_TYPE.ALLEGRO_VERTEX_SHADER
+		);
+
+		return Shader.ofShaders(vertexShaderSource, toStringz(fragmentShaderSource));
+	}
+
+	static Shader ofVertex(string vertexShaderSource) {
+		const char *fragmentShaderSource = al_get_default_shader_source(
+			ALLEGRO_SHADER_PLATFORM.ALLEGRO_SHADER_AUTO, 
+			ALLEGRO_SHADER_TYPE.ALLEGRO_PIXEL_SHADER
+		);
+
+		return Shader.ofShaders(toStringz(vertexShaderSource), fragmentShaderSource);		
+	}
+
+	static Shader ofShaders(string vertexShaderSource, string fragmentShaderSource) {
+		return Shader.ofShaders(toStringz(vertexShaderSource), toStringz(fragmentShaderSource));
+	}
+	
+	private static Shader ofShaders(const char* vertexShaderSource, const char *fragmentShaderSource) {
+		ALLEGRO_SHADER *shader = null;
+		
+		string raise(string msg, string file= __FILE__, size_t line = __LINE__) {
+			throw new ShaderException(format("%s: %s\n", msg, to!string(al_get_shader_log(shader))), file, line);
+		}
+
+		shader = al_create_shader(ALLEGRO_SHADER_PLATFORM.ALLEGRO_SHADER_AUTO);
+		if(!shader) raise("al_create_shader failed");
+
+		bool ok = al_attach_shader_source(shader, ALLEGRO_SHADER_TYPE.ALLEGRO_PIXEL_SHADER, fragmentShaderSource);
+		if (!ok) raise("al_attach_shader_source failed");
+
+		ok = al_attach_shader_source(shader, ALLEGRO_SHADER_TYPE.ALLEGRO_VERTEX_SHADER, vertexShaderSource);
+		if (!ok) raise("al_attach_shader_source failed");
+
+		ok = al_build_shader(shader);
+		if (!ok) raise("al_build_shader failed");
+		assert(ok);
+
+		Shader result = new Shader();
+		result.shader = shader;
+		return result;
+	}
+
+	//TODO: add facility for al_set_shader*
+	void use(bool enabled = true) {
+		if (enabled) {
+			al_use_shader(shader);
+		}
+		else {
+			al_use_shader(null);
+		}
+	}
+
+}
